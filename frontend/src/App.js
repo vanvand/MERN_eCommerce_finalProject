80de7f945fd34f1c034e71c10b644a98bfd47d7b
vanvand <vanessa.domas@gmail.com>
2022-09-27 08:21:35 +0200

    adding frontend components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
+import { Routes, Route} from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to ProShop</h1>
-
-    </div>
-  );
+    <>
+    <Header/>
+    <main className="py-3">
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>} />
+          <Route path="/product/:id" element={<ProductScreen/>} />
+        </Routes>
+      </Container>
+    </main>
+    <Footer/>
+    </>
+  )
 }
 
 export default App;
