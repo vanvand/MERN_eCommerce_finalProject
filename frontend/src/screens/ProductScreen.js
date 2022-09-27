7f33016df079b42365c5d39cefb09e8261af3e12
vanvand <vanessa.domas@gmail.com>
2022-09-27 11:09:19 +0200

    fix: use useParams instead of depricated match props to render ProductScreen corretly

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap"
 import Rating from "../components/Rating"
 import products from "../products"
 
-const ProductScreen = ({ match }) => {
-
-  const product = products.find((p) => p._id === match.params.id )
+const ProductScreen = () => {
+  const params = useParams();
+  const product = products.find((p) => p._id === params.id )
 
   return (
     <>
