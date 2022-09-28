2c8a71edd63c6541b0188f7aa824d2c981f7ff37
vanvand <vanessa.domas@gmail.com>
2022-09-28 13:05:52 +0200

    Set-UP Redux Store (with depricated createStore)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux"
+import store from "./store"
 import { BrowserRouter as Router} from "react-router-dom"
 import "./bootstrap.min.css";
 import './index.css';
@@ -8,11 +10,13 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+// import { configureStore } from '@reduxjs/toolkit'
+
+// const store = configureStore({ reducer: {} })
+
+const reducer = combineReducers({})
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
