1ea68cc69261217a0b754c4f91777572e4074e3e
vanvand <vanessa.domas@gmail.com>
2022-09-28 19:40:50 +0200

    tried to implement configureStore

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -19,3 +19,25 @@ const store = createStore(
 )
 
 export default store
+
+// try to implement configureStore
+// BUG: productDetails reducer is not called 
+// import { configureStore } from '@reduxjs/toolkit'
+// import { combineReducers } from "redux"
+// import thunk from 'redux-thunk'
+// import { productListReducer, productDetailReducer } from "./reducers/productReducers"
+
+// const reducer = combineReducers({
+//     productList: productListReducer,
+//     productDetails: productDetailReducer,
+// })
+
+// const store = configureStore({
+//   reducer,
+//   preloadedState: {},
+//   middleware: [thunk],
+// })
+
+// console.log(store.getState())
+
+// export default store
