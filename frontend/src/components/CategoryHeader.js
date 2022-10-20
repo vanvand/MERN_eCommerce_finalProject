ee9efa92f7e398ddce7c843f0eeebfb13fb0e152
Diala-Sleem <diala.slim@yahoo.com>
2022-10-20 17:06:04 +0200

    categoryHeader/update

--- frontend/src/components/CategoryHeader.js
+++ frontend/src/components/CategoryHeader.js
@@ -1,22 +1,52 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import "./components_css/categoryHeader.css";
-
-
 import { Link } from "react-router-dom";
-function CategoryHeader() {
-  const category = ["Tools", "Books", "Clothes"];
+import Message from "./Message";
+import Loader from "./Loader";
+import {useSelector } from "react-redux";
+
+function CategoryHeader () {
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products} = productList;
+
+  
+  const categories = products.map((product) => {
+    return product.category;
+  });
+    const filterCategory = categories.reduce(
+      (acc, categoryName) =>
+        acc.includes(categoryName) ? acc : [...acc, categoryName],
+      []
+    );
+
+  //console.log(" filterCategory", filterCategory);
+ 
+
   return (
     <Container className=" square border-bottom  sticky ">
-      <Row d-flex='true' flex-row='true'>
-        {category.map((name, index) => (
-          <Col key={index} className="alignStart">
-            <Link to={`/${name}`} className=" linkStyling ">
-              {name}
-            </Link>
-          </Col>
-        ))}
-      </Row>
+      {
+        loading ? (
+          <Loader />
+        ) : 
+        error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Row d-flex="true" flex-row="true">
+            {filterCategory.length > 0 &&
+              filterCategory.map((category, index) => (
+                <Col key={index} className="alignStart">
+                  <Link
+                    to={`/products/category/${category.toLowerCase()}`}
+                    className=" linkStyling "
+                  >
+                    {category}
+                  </Link>
+                </Col>
+              ))}
+          </Row>
+        )
+      }
     </Container>
   );
 }
