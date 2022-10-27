697eae035fef6abd0995fb6cb0246129d9ebfd97
Diala-Sleem <diala.slim@yahoo.com>
2022-10-27 18:07:17 +0200

    keep only the image, title and city add css to resize the image and hide the overflow title,

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,39 +2,30 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import { Card } from 'react-bootstrap'
 import Rating from "./Rating"
+import "./components_css/product.css";
 
 
 const Product = ({ product }) => { // destructure products passed as prop to use directly (alternative pass in (props) > props.product.id)
   return (
-    <Card className="my-2 p-3 rounded">
-        
+    <Card className="my-2 p-3 rounded" key={product._id}>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" className="productImage" />
+      </Link>
+
+      <Card.Body>
+        {/* it was h3 change it to h6 */}
+        <Card.Text as="h6">
+          <i className="fa-solid fa-location-dot"></i> 00000 City
+        </Card.Text>
+
         <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant="top"/>
+          <Card.Title as="h6" className="productText">
+            {product.name}
+          </Card.Title>
         </Link>
-
-        <Card.Body>
-            
-            <Link to={`/product/${product._id}`}>
-                <Card.Title as="div">
-                    <strong>{product.name}</strong>
-                </Card.Title>
-            </Link>
-
-            <Card.Text as="div">
-                <Rating 
-                    value={product.rating}
-                    text={`${product.numReviews} 
-                    reviews`}
-                />
-            </Card.Text>
-           {/* it was h3 change it to h5 */}
-            <Card.Text as="h5">
-                ${product.price}
-            </Card.Text>
-
-        </Card.Body>
+      </Card.Body>
     </Card>
-  )
+  );
 }
 
 export default Product
