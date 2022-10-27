1e525d846d1ace0e285f5be5ccbba122cbdc8c6d
Diala-Sleem <diala.slim@yahoo.com>
2022-10-27 15:02:51 +0200

    add userAddWishItemReducer,userWishListReducer and userDeleteWishItemReducer to userReducers

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,31 +1,41 @@
-import { 
-    USER_LOGIN_FAIL, 
-    USER_LOGIN_REQUEST, 
-    USER_LOGIN_SUCCESS, 
-    USER_LOGOUT, 
-    USER_REGISTER_REQUEST, 
-    USER_REGISTER_SUCCESS, 
-    USER_REGISTER_FAIL,
-    USER_DETAILS_REQUEST,
-    USER_DETAILS_SUCCESS,
-    USER_DETAILS_FAIL,
-    USER_DETAILS_RESET,
-    USER_UPDATE_PROFILE_REQUEST,
-    USER_UPDATE_PROFILE_SUCCESS,
-    USER_UPDATE_PROFILE_FAIL,
-    USER_UPDATE_PROFILE_RESET,
-    USER_LIST_REQUEST,
-    USER_LIST_SUCCESS,
-    USER_LIST_FAIL,
-    USER_LIST_RESET,
-    USER_DELETE_REQUEST,
-    USER_DELETE_SUCCESS,
-    USER_DELETE_FAIL,
-    USER_UPDATE_REQUEST,
-    USER_UPDATE_SUCCESS,
-    USER_UPDATE_FAIL,
-    USER_UPDATE_RESET
-} from "../constants/userConstants"
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_RESET,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_RESET,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
+  USER_LIST_RESET,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+  USER_UPDATE_FAIL,
+  USER_UPDATE_RESET,
+  //wishList
+  USER_ADD_WISHITEM_REQUEST,
+  USER_ADD_WISHITEM_SUCCESS,
+  USER_ADD_WISHITEM_FAIL,
+  USER_WISHLIST_REQUEST,
+  USER_WISHLIST_SUCCESS,
+  USER_WISHLIST_FAIL,
+  USER_DELETE_WISHITEM_REQUEST,
+  USER_DELETE_WISHITEM_SUCCESS,
+  USER_DELETE_WISHITEM_FAIL,
+} from "../constants/userConstants";
 
 
 export const userLoginReducer = (state = { }, action) => {
@@ -162,3 +172,80 @@ export const userUpdateReducer = (state = { user: {} }, action) => {
     }
 }
 
+
+  //wishList
+// USER_ADD_WISHITEM_REQUEST ,
+// USER_ADD_WISHITEM_SUCCESS ,
+// USER_ADD_WISHITEM_FAIL ,
+
+export const userAddWishItemReducer = (state = { wishItems: [] }, action) => {
+  switch (action.type) {
+      case USER_ADD_WISHITEM_REQUEST:
+           const item = action.payload 
+            const existItem = state.wishItems.find(
+              (x) => x.product === item.product
+            );
+            
+            if(existItem) {
+                return {
+                  ...state,
+                  wishItems: state.wishItems.map((x) =>
+                    x.product === existItem.product ? item : x
+                  ),
+                };
+            } else {
+                return  { ...state, loading: true }
+            }
+              
+         //return { ...state, loading: true };
+    case USER_ADD_WISHITEM_SUCCESS:
+      return { loading: false, wishItems: action.payload };
+    case USER_ADD_WISHITEM_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+
+// USER_WISHLIST_REQUEST,
+// USER_WISHLIST_SUCCESS,
+// USER_WISHLIST_FAIL,
+
+
+export const userWishListReducer = (state = { wishItems: [] }, action) => {
+  switch (action.type) {
+    case USER_WISHLIST_REQUEST:
+      return { ...state, loading: true };
+    case USER_WISHLIST_SUCCESS:
+      return { loading: false, wishItems: action.payload };
+    case USER_WISHLIST_FAIL:
+      return { loading: false, error: action.payload };
+
+
+    default:
+      return state;
+  }
+};
+
+
+
+// USER_DELETE_WISHITEM_REQUEST 
+// USER_DELETE_WISHITEM_SUCCESS 
+// USER_DELETE_WISHITEM_FAIL 
+
+
+export const userDeleteWishItemReducer = (state = { wishItems: [] }, action) => {
+  switch (action.type) {
+    case USER_DELETE_WISHITEM_REQUEST:
+      return {  loading: true };
+    case USER_DELETE_WISHITEM_SUCCESS:
+      return { loading: false, success: true };
+    case USER_DELETE_WISHITEM_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
