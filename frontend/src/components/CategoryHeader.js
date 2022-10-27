8ad82181b3bf102c516a4fa51cd56c7f0eed8e4a
Diala-Sleem <diala.slim@yahoo.com>
2022-10-27 21:16:36 +0200

    connecte CategoryHeader with ProductsByCategoryScreen

--- frontend/src/components/CategoryHeader.js
+++ frontend/src/components/CategoryHeader.js
@@ -1,22 +1,60 @@
-import React from "react";
-import { Container, Col, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+  Container,
+  Col,
+  Row,
+  Form,
+  Button,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
 import "./components_css/categoryHeader.css";
-
-
+import Message from "./Message";
+import Loader from "./Loader";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+
 function CategoryHeader() {
-  const category = ["Tools", "Books", "Clothes"];
+  const productList = useSelector((state) => {
+    //console.log("state.productList", state.productList);
+    return state.productList;
+  });
+  const { loading, error, allProductsCategory } = productList;
+
+  const categories = [
+    ...new Set(
+      allProductsCategory.map((product) => {
+        return product.category;
+      })
+    ),
+  ];
+
   return (
     <Container className=" square border-bottom  sticky ">
-      <Row d-flex='true' flex-row='true'>
-        {category.map((name, index) => (
-          <Col key={index} className="alignStart">
-            <Link to={`/${name}`} className=" linkStyling ">
-              {name}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Nav className="ml-auto ">
+          {categories.map((category, index) => (
+            <Link
+              key={index}
+              to={`/products/category/${category}`}
+              className=" linkStyling "
+            >
+              <Button
+                variant="outline-light"
+                size="xs"
+                type="submit"
+                className="p-0 categoryBut"
+              >
+                {category}
+              </Button>
             </Link>
-          </Col>
-        ))}
-      </Row>
+          ))}
+        </Nav>
+      )}
     </Container>
   );
 }
