fb1e2c8025c7c41908b9302dd06cbd6e5eed1f6e
Diala-Sleem <diala.slim@yahoo.com>
2022-10-27 20:28:18 +0200

    example for PrivacyPolicy,TermsOfUse data

--- frontend/src/components/footer/footerData.js
+++ frontend/src/components/footer/footerData.js
@@ -1,19 +1,17 @@
 const CommunityGuidelines = {
   name: "Community Guidelines",
   title: "Community Guidelines",
-    text: "Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.",
+  text: "Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.",
 };
 const PrivacyPolicy = {
   name: "Privacy Policy",
   title: "Privacy Policy",
-  text: "Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc",
+  text: "PROSHOP respect the privacy of our users. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website PROSHOP. Please read this privacy policy carefully. If you disagree with the terms of this privacy policy, please do not access the site. We reserve the right to make changes to this Privacy Policy at any time and for any reason. We will alert you about any changes by updating the Last Updated date of this Privacy Policy. Any changes or modifications will be effective immediately upon posting the updated Privacy Policy on the Site. You waive the right to receive specific notice of each such change or modification. You are encouraged to periodically review this Privacy Policy to stay informed of updates. You will be deemed to have been made aware of, will be subject to, and will be deemed to have accepted the changes in any revised Privacy Policy by your continued use of the Site after the date such revised Privacy Policy is posted. ",
 };
 const TermsOfUse = {
   name: " Terms Of Use",
   title: "Terms Of Use",
-  text: "Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.",
+  text: "Please read these Terms of Use, and the documents referred to below, carefully before you start to use our site, as they apply to your use of our site. By using our site, you confirm that you accept these Terms of Use and the documents referred to below to the exclusion of all other terms and conditions and that you agree to comply with them. If you do not agree to these Terms of Use or the documents referred to below, you must not use our site",
 };
-  
-
 
 export { CommunityGuidelines, PrivacyPolicy, TermsOfUse };
