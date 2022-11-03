8f8699a4985200c2526c880ca2644d2f33006ba2
Diala-Sleem <diala.slim@yahoo.com>
2022-11-03 22:11:25 +0100

    update availability button

--- frontend/src/screens/UserAdsScreen.js
+++ frontend/src/screens/UserAdsScreen.js
@@ -11,7 +11,7 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { getUserDetails } from "../actions/userActions";
 import "../components/components_css/myAddScreen.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   deleteProduct,
   updateProduct,
@@ -21,12 +21,9 @@ import { LinkContainer } from "react-router-bootstrap";
 import UserDetails from "../components/UserDetails";
 
 export default function UserAdsScreen() {
-  const navigate = useNavigate();
-
   const [availability, setAvailability] = useState();
   const [product, setProduct] = useState();
   const [productId, setProductId] = useState();
-
   const dispatch = useDispatch();
 
  
@@ -35,7 +32,7 @@ export default function UserAdsScreen() {
   const { loading: userLoading, error: userError, user } = userDetails;
 
   const productDelete = useSelector((state) => {
-    console.log("productDelete", state.productDelete);
+   // console.log("productDelete", state.productDelete);
 
     return state.productDelete;
   });
@@ -53,20 +50,20 @@ export default function UserAdsScreen() {
   //console.log(allProductsCategory);
 
   const productUpdate = useSelector((state) => {
-    //console.log('state.productUpdat',state);
+   // console.log('state.productUpdat',state);
     return state.productUpdate;
   });
   const { product: productUpdatesuccess } = productUpdate;
 
   const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
+  const { userInfo, loading } = userLogin;
 
   
 
   useEffect(() => {
     if (!user || !user.name) {
       // hit /api/users/profile in userActions
-      console.log("userInfo", userInfo);
+      //console.log("userInfo", userInfo);
       dispatch(getUserDetails("profile"));
     }
   }, [dispatch, user]);
@@ -78,14 +75,18 @@ export default function UserAdsScreen() {
   };
 
   useEffect(() => {
-    if (product) {
-      dispatch(updateProduct({ ...product, availability: availability }));
-    }
+      if (product) {
+        dispatch(
+          updateProduct({
+            ...product,
+            availability: availability,
+          })
+        );
+      }
   }, [availability, dispatch]);
 
   useEffect(() => {
     dispatch(listProducts());
-    console.log("Hi it");
   }, [productUpdatesuccess, deleteSuccess]);
 
   const availabilityHandeler = (productId) => {
@@ -96,11 +97,12 @@ export default function UserAdsScreen() {
       });
       setProduct(product);
       setAvailability(!product.availability);
+      
     }
   };
   return (
     <>
-      {userLoading && productLoading && <Loader />}
+      {userLoading && productLoading && loading && <Loader />}
       {userError && <Message variant="danger">{userError}</Message>}
       {productLoading ? (
         <Loader />
@@ -108,69 +110,70 @@ export default function UserAdsScreen() {
         <Message variant="danger">{error}</Message>
       ) : (
         <Container>
-          <Row className=" flex-md-row  ">
-            <Row className=" flex-md-row  my-3 userAdd">
+          <Row className=" flex-md-row  " md={2}>
+            <Col>
               <UserDetails />
-            </Row>
-            <Row>
-              {allProductsCategory.map((product) => (
-                <>
-                  {user._id === product.user && user._id === userInfo._id && (
-                    <Card className="my-2 p-3 rounded " key={product._id}>
-                      <Row>
-                        <Col sm={12} lg={5}>
-                          {" "}
-                          <Link to={`/product/${product._id}`}>
-                            {product.availability ? (
+            </Col>
+          </Row>
+
+          <Row className=" flex-md-row  ">
+            {allProductsCategory.map((product) => (
+              <>
+                {user._id === product.user && user._id === userInfo._id && (
+                  <Card className="my-2 p-3 rounded " key={product._id}>
+                    <Row>
+                      <Col sm={12} lg={3}>
+                        {" "}
+                        <Link to={`/product/${product._id}`}>
+                          {product.availability ? (
+                            <Card.Img
+                              src={product.image}
+                              variant="top"
+                              className="productImage userAddImage"
+                            />
+                          ) : (
+                            <Card>
                               <Card.Img
                                 src={product.image}
                                 variant="top"
-                                className="productImage"
+                                className="productImage opacity-25 userAddImage"
                               />
-                            ) : (
-                              <Card>
-                                <Card.Img
-                                  src={product.image}
-                                  variant="top"
-                                  className="productImage opacity-25 "
-                                />
-                                <Card.ImgOverlay>
-                                  <div className=" bg-dark mt-5 p-1 text-center text-danger">
-                                    <h6 className="fa-solid fa-rotate">
-                                      Rented
-                                    </h6>
-                                  </div>
-                                </Card.ImgOverlay>
-                              </Card>
-                            )}
+                              <Card.ImgOverlay>
+                                <div className=" bg-dark mt-5 p-1 text-center text-danger">
+                                  <h6 className="fa-solid fa-rotate">Rented</h6>
+                                </div>
+                              </Card.ImgOverlay>
+                            </Card>
+                          )}
+                        </Link>
+                      </Col>
+                      <Col sm={12} lg={7}>
+                        <Card.Body>
+                          <Card.Text as="h6">{product.category}</Card.Text>
+
+                          <Link to={`/product/${product._id}`}>
+                            <Card.Title as="h5" className=" mb-3">
+                              {product.name}
+                            </Card.Title>
                           </Link>
-                        </Col>
-                        <Col sm={12} lg={5}>
-                          <Card.Body>
-                            <Card.Text as="h6">{product.category}</Card.Text>
 
-                            <Link to={`/product/${product._id}`}>
-                              <Card.Title as="h5" className="productText mb-3">
-                                {product.name}
-                              </Card.Title>
-                            </Link>
+                          <Card.Text as="h6" className="productText">
+                            Created on: {product.createdAt.substring(0, 10)}
+                          </Card.Text>
+                          <Card.Text as="h6">
+                            ({product.timesRented})Times rented
+                          </Card.Text>
+                        </Card.Body>
+                      </Col>
+                      <Col sm={12} lg={2}>
+                        <Row>
+                          <LinkContainer to={`/products/${product._id}/edit`}>
+                            <Button variant="dark" className="btn-sm mb-1">
+                              <i className="fas fa-edit"></i> Edit
+                            </Button>
+                          </LinkContainer>
 
-                            <Card.Text as="h6" className="productText">
-                              Created on: {product.createdAt.substring(0, 10)}
-                            </Card.Text>
-                            <Card.Text as="h6">
-                              ({product.timesRented})Times rented
-                            </Card.Text>
-                          </Card.Body>
-                        </Col>
-                        <Col sm={12} lg={2}>
-                          <Row>
-                            <LinkContainer to={`/products/${product._id}/edit`}>
-                              <Button variant="dark" className="btn-sm mb-1">
-                                <i className="fas fa-edit"></i> Edit
-                              </Button>
-                            </LinkContainer>
-
+                          {product.availability == false ? (
                             <Button
                               variant="dark"
                               className="btn-sm mb-1"
@@ -179,21 +182,29 @@ export default function UserAdsScreen() {
                               <i className="fa-solid fa-rotate"></i> Set
                               Available
                             </Button>
+                          ) : (
                             <Button
                               variant="dark"
                               className="btn-sm mb-1"
-                              onClick={() => deleteHandler(product._id)}
+                              onClick={() => availabilityHandeler(product._id)}
                             >
-                              <i className="fas fa-trash"></i> Delete
+                              <i className="fa-solid fa-signal"></i> Online
                             </Button>
-                          </Row>
-                        </Col>
-                      </Row>
-                    </Card>
-                  )}
-                </>
-              ))}
-            </Row>
+                          )}
+                          <Button
+                            variant="dark"
+                            className="btn-sm mb-1"
+                            onClick={() => deleteHandler(product._id)}
+                          >
+                            <i className="fas fa-trash"></i> Delete
+                          </Button>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Card>
+                )}
+              </>
+            ))}
           </Row>
         </Container>
       )}
