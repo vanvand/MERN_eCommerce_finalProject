de3411f0cdb48cd724694639f487faaac299605d
Diala-Sleem <diala.slim@yahoo.com>
2022-11-04 23:26:24 +0100

    upload 3 images remove price and brand

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,18 +1,20 @@
 import mongoose from "mongoose";
 
-const reviewSchema = mongoose.Schema({
-    name: {type: String,required: true},
-    rating: {type: Number,required: true},
-    comment: {type: String,required: true},
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User"
-    }
-}, {
-    timestamps: true
-})
-
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const productSchema = mongoose.Schema(
   {
@@ -60,7 +62,6 @@ const productSchema = mongoose.Schema(
     },
     timesRented: {
       type: Number,
-      required: true,
       default: 0,
     },
     rentedTo: {
@@ -73,6 +74,6 @@ const productSchema = mongoose.Schema(
   }
 );
 
-const Product = mongoose.model("Product", productSchema)
+const Product = mongoose.model("Product", productSchema);
 
-export default Product
+export default Product;
